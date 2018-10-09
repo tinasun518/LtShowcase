39e8e954d2b248c2c3fbf22519268b8397c22cf1
Yanming Sun <yanmsun@healthcare.uiowa.edu>
2018-10-09 10:34:15 -0500

    initial commit

---
+++ LtShowcase/Photo.cs
@@ -0,0 +1,11 @@
+﻿namespace LtShowcase
+{
+    public class Photo
+    {
+        public string albumId { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string thumbnailUrl { get; set; }
+    }
+}
---
+++ LtShowcase/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace LtShowcase
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var photos = new List<Photo>();
+                using (var client = new WebClient())
+                {
+                    var json = client.DownloadString("https://jsonplaceholder.typicode.com/photos");
+                    photos = new JavaScriptSerializer().Deserialize<List<Photo>>(json);
+                }
+                var input = string.Empty;
+                do
+                {
+                    input = Console.ReadLine();
+                    var command = input?.Split();
+                    if (string.Equals(input.ToLower(), "quit"))
+                    {
+                        break;
+                    }
+                    if (command.Length != 2)
+                    {
+                        Console.WriteLine("Command not valid.");
+                        continue;
+                    }
+                    if (string.Equals(command[0].ToLower(), "photo-album"))
+                    {
+                        var selectedPhotos = photos.FindAll(photo => photo.albumId == command[1]);
+                        foreach (var photo in selectedPhotos)
+                        {
+                            Console.WriteLine($"[{photo.id}] {photo.title.ToString()}");
+                        }
+                    }
+                }
+                while (!string.Equals(input.ToLower(), "quit"));
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+        }
+    }
+}
